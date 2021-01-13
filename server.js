8ee6017769b286e1d953d29e9814e2d939a5710a
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2021-01-13 16:20:03 -0500

    set up mongoose server.

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require("express");
-const mongojs = require("mongojs");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const databaseUrl = "fitness";
-const collections = ["exercises"];
+app.use(morgan("dev"));
 
-const db = mongojs(databaseUrl, collections);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-db.on("error", error => {
-    console.log("Database Error:", error);
-});
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb",
+    { useNewUrlParser: true, useFindAndModify: false });
+
+// db.on("error", error => {
+//     console.log("Database Error:", error);
+// });
 
 // Set the app to listen on port 3000
 app.listen(3000, () => {
-    console.log("App running on port 3000!");
+    console.log("App running on port", PORT);
 });
