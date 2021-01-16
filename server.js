925191509f9477a9900444782725ebc855779266
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2021-01-16 09:58:47 -0500

    setting up app with atlas

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.use(require("./routes/html-routes"));
 
 
 // Set the app to listen on port 3000
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("App running on port", PORT);
 });
